21573610d5dbd3d9bca180e2558880969d542e6e
Alfred <70281638+CodeWithAlfred@users.noreply.github.com>
2025-01-14 15:36:16 +0300

    Update main.py
    Added the parameters for status description based on three values: todo, in-progress, and done

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ class Task:
     def __init__(self, id, desc, status=None, created_at, updated_at):
         self.id = id
         self.desc = desc
-        self.status_values = ["todo", "In progress", "done"]
+         self.status_values = ["todo", "In progress", "done"]
+        if status is None: 
+            self.status = self.status_value[0]
+        elif status in status_values:
+            self.status = status
+        else:
+            raise ValueError(f"Invalid  status description. Choose from: {', '.join(self.status_values)}")
+       
         self.created_at = created_at
         self.updated_at = updated_at
